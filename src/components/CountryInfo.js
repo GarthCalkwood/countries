7939ffe0882350a8f15d52e6ef9ab3c6d60d77b4
Garth Calkwood <garth.calkwood@gmail.com>
2021-08-14 10:58:17 -0500

    Added feature to display detailed info on a searched country

---
+++ src/components/CountryInfo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const CountryInfo = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <div>
+        Capital: {country.capital} <br/>
+        Population: {country.population}
+      </div>
+      <h3>Languages</h3>
+      <ul>
+        {country.languages.map((language) => <li>{language.name}</li>)}
+      </ul>
+      <img src={country.flag} alt={`Flag of ${country.name}`} width={300} height={200}/>
+    </div>
+    
+  )
+}
+
+export default CountryInfo
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import CountryInfo from './CountryInfo'
 
 const SearchResults = ({results}) => {
-  
+  console.log('results: ', results)
+
+  if (results.length === 0){
+    return <div>No Results</div>
+  }
+
+  if (results.length === 1){
+    console.log('results[0]: ', results[0])
+    return <CountryInfo country={results[0]}/>
+  }
+
+
   const output = results.map(result => 
     <div key={result.name}>
       {result.name}
